ca27084b1c52c4388aab1a7ee97bdbb843b4ae5b
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-11 11:27:04 -0400

    Created store and reducer

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,20 @@
+
+const initialState ={
+smurfs:[]
+
+
+}
+
+const reducer = (state = initialState, action) => {
+switch (action.type) {
+
+
+
+    default:
+        return state;
+}
+
+
+}
+
+export default reducer;
